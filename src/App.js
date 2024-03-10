459aec28790e0385ebe75e11539852d7b4dde4dd
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-10 05:25:20 +0200

    build steps component and implement conditional styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
 
-function App() {
+const App = () => {
+  const step = 2;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""} `}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''} `}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''} `}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
