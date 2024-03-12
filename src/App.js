9920ea3f857bff0f89ed686e29a61b9d0ebc3408
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-12 17:07:47 +0200

    add state to update component according to an event

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,11 +7,19 @@ const messages = [
 ];
 
 const App = () => {
-  const step = 2;
+  const [step, setStep] = useState(1);
+
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""} `}>1</div>
+        <div className={`${step >= 1 ? 'active' : ''} `}>1</div>
         <div className={`${step >= 2 ? 'active' : ''} `}>2</div>
         <div className={`${step >= 3 ? 'active' : ''} `}>3</div>
       </div>
@@ -18,10 +28,16 @@ const App = () => {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
